96e90a31dff2d4e78481be91cba981e8970f675e
Jessica Weber <58643745+JessicaMWeber@users.noreply.github.com>
2019-12-19 17:44:45 -0600

    Add files via upload

---
+++ CookieClicker/GameForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace CookieClicker
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.pbLemon_Cookie = new System.Windows.Forms.PictureBox();
+            this.lblScore_Number = new System.Windows.Forms.Label();
+            this.lblCookie_Name = new System.Windows.Forms.Label();
+            this.btnDirections = new System.Windows.Forms.Button();
+            this.pbMM_Cookie = new System.Windows.Forms.PictureBox();
+            this.pbGinger = new System.Windows.Forms.PictureBox();
+            this.pbSugar = new System.Windows.Forms.PictureBox();
+            this.pbFrosted = new System.Windows.Forms.PictureBox();
+            this.btnsugar = new System.Windows.Forms.Button();
+            this.btnmm = new System.Windows.Forms.Button();
+            this.btnfrost = new System.Windows.Forms.Button();
+            this.btnginger = new System.Windows.Forms.Button();
+            this.btnlemon = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.pcChoc_Cookie = new System.Windows.Forms.PictureBox();
+            this.btnChoc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLemon_Cookie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMM_Cookie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGinger)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSugar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFrosted)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcChoc_Cookie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbLemon_Cookie
+            // 
+            this.pbLemon_Cookie.BackColor = System.Drawing.Color.Transparent;
+            this.pbLemon_Cookie.Image = ((System.Drawing.Image)(resources.GetObject("pbLemon_Cookie.Image")));
+            this.pbLemon_Cookie.Location = new System.Drawing.Point(360, 114);
+            this.pbLemon_Cookie.Name = "pbLemon_Cookie";
+            this.pbLemon_Cookie.Size = new System.Drawing.Size(396, 381);
+            this.pbLemon_Cookie.TabIndex = 0;
+            this.pbLemon_Cookie.TabStop = false;
+            this.pbLemon_Cookie.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pbLemon_Cookie.DoubleClick += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // lblScore_Number
+            // 
+            this.lblScore_Number.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore_Number.Location = new System.Drawing.Point(536, 498);
+            this.lblScore_Number.Name = "lblScore_Number";
+            this.lblScore_Number.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblScore_Number.Size = new System.Drawing.Size(226, 56);
+            this.lblScore_Number.TabIndex = 2;
+            this.lblScore_Number.Text = "0";
+            // 
+            // lblCookie_Name
+            // 
+            this.lblCookie_Name.AutoSize = true;
+            this.lblCookie_Name.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCookie_Name.ForeColor = System.Drawing.Color.Turquoise;
+            this.lblCookie_Name.Location = new System.Drawing.Point(311, 19);
+            this.lblCookie_Name.Name = "lblCookie_Name";
+            this.lblCookie_Name.Size = new System.Drawing.Size(447, 84);
+            this.lblCookie_Name.TabIndex = 3;
+            this.lblCookie_Name.Text = "Chocolate Chip";
+            // 
+            // btnDirections
+            // 
+            this.btnDirections.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnDirections.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDirections.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDirections.ForeColor = System.Drawing.Color.White;
+            this.btnDirections.Location = new System.Drawing.Point(37, 35);
+            this.btnDirections.Name = "btnDirections";
+            this.btnDirections.Size = new System.Drawing.Size(214, 56);
+            this.btnDirections.TabIndex = 4;
+            this.btnDirections.Text = "Directions";
+            this.btnDirections.UseVisualStyleBackColor = false;
+            this.btnDirections.Click += new System.EventHandler(this.BtnDirections_Click);
+            // 
+            // pbMM_Cookie
+            // 
+            this.pbMM_Cookie.Image = ((System.Drawing.Image)(resources.GetObject("pbMM_Cookie.Image")));
+            this.pbMM_Cookie.Location = new System.Drawing.Point(360, 114);
+            this.pbMM_Cookie.Name = "pbMM_Cookie";
+            this.pbMM_Cookie.Size = new System.Drawing.Size(396, 381);
+            this.pbMM_Cookie.TabIndex = 7;
+            this.pbMM_Cookie.TabStop = false;
+            this.pbMM_Cookie.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pbMM_Cookie.DoubleClick += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pbGinger
+            // 
+            this.pbGinger.Image = ((System.Drawing.Image)(resources.GetObject("pbGinger.Image")));
+            this.pbGinger.Location = new System.Drawing.Point(360, 114);
+            this.pbGinger.Name = "pbGinger";
+            this.pbGinger.Size = new System.Drawing.Size(396, 381);
+            this.pbGinger.TabIndex = 8;
+            this.pbGinger.TabStop = false;
+            this.pbGinger.Visible = false;
+            this.pbGinger.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pbGinger.DoubleClick += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pbSugar
+            // 
+            this.pbSugar.Image = ((System.Drawing.Image)(resources.GetObject("pbSugar.Image")));
+            this.pbSugar.Location = new System.Drawing.Point(360, 114);
+            this.pbSugar.Name = "pbSugar";
+            this.pbSugar.Size = new System.Drawing.Size(396, 381);
+            this.pbSugar.TabIndex = 9;
+            this.pbSugar.TabStop = false;
+            this.pbSugar.Visible = false;
+            this.pbSugar.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pbSugar.DoubleClick += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pbFrosted
+            // 
+            this.pbFrosted.Image = ((System.Drawing.Image)(resources.GetObject("pbFrosted.Image")));
+            this.pbFrosted.Location = new System.Drawing.Point(360, 114);
+            this.pbFrosted.Name = "pbFrosted";
+            this.pbFrosted.Size = new System.Drawing.Size(396, 381);
+            this.pbFrosted.TabIndex = 10;
+            this.pbFrosted.TabStop = false;
+            this.pbFrosted.Visible = false;
+            this.pbFrosted.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pbFrosted.DoubleClick += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // btnsugar
+            // 
+            this.btnsugar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnsugar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsugar.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsugar.ForeColor = System.Drawing.Color.White;
+            this.btnsugar.Location = new System.Drawing.Point(34, 344);
+            this.btnsugar.Name = "btnsugar";
+            this.btnsugar.Size = new System.Drawing.Size(217, 56);
+            this.btnsugar.TabIndex = 15;
+            this.btnsugar.Text = "Sugar";
+            this.btnsugar.UseVisualStyleBackColor = false;
+            this.btnsugar.Click += new System.EventHandler(this.Btnsugar_Click);
+            // 
+            // btnmm
+            // 
+            this.btnmm.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnmm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnmm.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmm.ForeColor = System.Drawing.Color.White;
+            this.btnmm.Location = new System.Drawing.Point(34, 421);
+            this.btnmm.Name = "btnmm";
+            this.btnmm.Size = new System.Drawing.Size(217, 56);
+            this.btnmm.TabIndex = 14;
+            this.btnmm.Text = "M+M";
+            this.btnmm.UseVisualStyleBackColor = false;
+            this.btnmm.Click += new System.EventHandler(this.Btnmm_Click);
+            // 
+            // btnfrost
+            // 
+            this.btnfrost.BackColor = System.Drawing.Color.HotPink;
+            this.btnfrost.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnfrost.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfrost.ForeColor = System.Drawing.Color.White;
+            this.btnfrost.Location = new System.Drawing.Point(34, 193);
+            this.btnfrost.Name = "btnfrost";
+            this.btnfrost.Size = new System.Drawing.Size(217, 56);
+            this.btnfrost.TabIndex = 13;
+            this.btnfrost.Text = "Frosted";
+            this.btnfrost.UseVisualStyleBackColor = false;
+            this.btnfrost.Click += new System.EventHandler(this.Btnfrost_Click);
+            // 
+            // btnginger
+            // 
+            this.btnginger.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnginger.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnginger.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnginger.ForeColor = System.Drawing.Color.White;
+            this.btnginger.Location = new System.Drawing.Point(34, 498);
+            this.btnginger.Name = "btnginger";
+            this.btnginger.Size = new System.Drawing.Size(217, 56);
+            this.btnginger.TabIndex = 12;
+            this.btnginger.Text = "Gingerbread";
+            this.btnginger.UseVisualStyleBackColor = false;
+            this.btnginger.Click += new System.EventHandler(this.Btnginger_Click);
+            // 
+            // btnlemon
+            // 
+            this.btnlemon.BackColor = System.Drawing.Color.Gold;
+            this.btnlemon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlemon.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlemon.ForeColor = System.Drawing.Color.White;
+            this.btnlemon.Location = new System.Drawing.Point(34, 114);
+            this.btnlemon.Name = "btnlemon";
+            this.btnlemon.Size = new System.Drawing.Size(217, 56);
+            this.btnlemon.TabIndex = 11;
+            this.btnlemon.Text = "Lemon";
+            this.btnlemon.UseVisualStyleBackColor = false;
+            this.btnlemon.Click += new System.EventHandler(this.Btnlemon_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(359, 498);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(171, 56);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "SCORE:";
+            // 
+            // pcChoc_Cookie
+            // 
+            this.pcChoc_Cookie.Image = ((System.Drawing.Image)(resources.GetObject("pcChoc_Cookie.Image")));
+            this.pcChoc_Cookie.Location = new System.Drawing.Point(360, 114);
+            this.pcChoc_Cookie.Name = "pcChoc_Cookie";
+            this.pcChoc_Cookie.Size = new System.Drawing.Size(396, 381);
+            this.pcChoc_Cookie.TabIndex = 16;
+            this.pcChoc_Cookie.TabStop = false;
+            this.pcChoc_Cookie.Visible = false;
+            this.pcChoc_Cookie.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pcChoc_Cookie.DoubleClick += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // btnChoc
+            // 
+            this.btnChoc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnChoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChoc.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoc.ForeColor = System.Drawing.Color.White;
+            this.btnChoc.Location = new System.Drawing.Point(34, 268);
+            this.btnChoc.Name = "btnChoc";
+            this.btnChoc.Size = new System.Drawing.Size(217, 56);
+            this.btnChoc.TabIndex = 17;
+            this.btnChoc.Text = "Chocolate Chip";
+            this.btnChoc.UseVisualStyleBackColor = false;
+            this.btnChoc.Click += new System.EventHandler(this.BtnChoc_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(869, 586);
+            this.Controls.Add(this.btnChoc);
+            this.Controls.Add(this.pcChoc_Cookie);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lblScore_Number);
+            this.Controls.Add(this.btnsugar);
+            this.Controls.Add(this.btnmm);
+            this.Controls.Add(this.btnfrost);
+            this.Controls.Add(this.btnginger);
+            this.Controls.Add(this.btnlemon);
+            this.Controls.Add(this.pbFrosted);
+            this.Controls.Add(this.pbSugar);
+            this.Controls.Add(this.pbGinger);
+            this.Controls.Add(this.pbMM_Cookie);
+            this.Controls.Add(this.btnDirections);
+            this.Controls.Add(this.lblCookie_Name);
+            this.Controls.Add(this.pbLemon_Cookie);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GameForm";
+            this.Text = "Click the Cookie";
+            ((System.ComponentModel.ISupportInitialize)(this.pbLemon_Cookie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMM_Cookie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGinger)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSugar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFrosted)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcChoc_Cookie)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbLemon_Cookie;
+        private System.Windows.Forms.Label lblScore_Number;
+        private System.Windows.Forms.Label lblCookie_Name;
+        private System.Windows.Forms.Button btnDirections;
+        private System.Windows.Forms.PictureBox pbMM_Cookie;
+        private System.Windows.Forms.PictureBox pbGinger;
+        private System.Windows.Forms.PictureBox pbSugar;
+        private System.Windows.Forms.PictureBox pbFrosted;
+        private System.Windows.Forms.Button btnsugar;
+        private System.Windows.Forms.Button btnmm;
+        private System.Windows.Forms.Button btnfrost;
+        private System.Windows.Forms.Button btnginger;
+        private System.Windows.Forms.Button btnlemon;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.PictureBox pcChoc_Cookie;
+        private System.Windows.Forms.Button btnChoc;
+    }
+}
+
---
+++ CookieClicker/GameForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookieClicker
+{
+    public partial class GameForm : System.Windows.Forms.Form
+    {
+        public string CookieName { get; set; }
+        int score = 0;
+        public GameForm()
+        {
+            InitializeComponent();
+            Set_Lemon_Cookie();
+        }
+
+
+
+        #region METHODS      
+
+
+        private void cookieclicker()
+        {
+            score = score + 1;
+            lblScore_Number.Text = score.ToString();
+        }
+
+        private void Set_MM_Cookie()
+        {
+            lblCookie_Name.Text = "M+M Cookie";
+            lblCookie_Name.ForeColor = Color.Blue;
+            pbMM_Cookie.Visible = true;
+            pbLemon_Cookie.Visible = false;
+            pbFrosted.Visible = false;
+            pbGinger.Visible = false;
+            pbSugar.Visible = false;
+            pcChoc_Cookie.Visible = false;
+        }
+
+        private void Set_Lemon_Cookie()
+        {
+            lblCookie_Name.Text = "Lemon Cookie";
+            lblCookie_Name.ForeColor = Color.Gold;
+            pbMM_Cookie.Visible = false;
+            pbLemon_Cookie.Visible = true;
+            pbFrosted.Visible = false;
+            pbGinger.Visible = false;
+            pbSugar.Visible = false;
+            pcChoc_Cookie.Visible = false;
+
+        }
+        private void Set_Frosted_Cookie()
+        {
+            lblCookie_Name.Text = "Frosted Cookie";
+            lblCookie_Name.ForeColor = Color.HotPink;
+            pbMM_Cookie.Visible = false;
+            pbLemon_Cookie.Visible = false;
+            pbFrosted.Visible = true;
+            pbGinger.Visible = false;
+            pbSugar.Visible = false;
+            pcChoc_Cookie.Visible = false;
+        }
+        public void Set_ginger_Cookie()
+        {
+            lblCookie_Name.Text = "Gingerbread Man";
+            lblCookie_Name.ForeColor = Color.Green;
+            pbMM_Cookie.Visible = false;
+            pbLemon_Cookie.Visible = false;
+            pbFrosted.Visible = false;
+            pbGinger.Visible = true;
+            pbSugar.Visible = false;
+            pcChoc_Cookie.Visible = false;
+        }
+        public void Set_Sugar_Cookie()
+        {
+            lblCookie_Name.Text = "Sugar Cookie";
+            lblCookie_Name.ForeColor = Color.Lavender;
+            pbMM_Cookie.Visible = false;
+            pbLemon_Cookie.Visible = false;
+            pbFrosted.Visible = false;
+            pbGinger.Visible = false;
+            pbSugar.Visible = true;
+            pcChoc_Cookie.Visible = false;
+        }
+
+        public void Set_Choc_Cookie()
+
+        {
+            lblCookie_Name.Text = "Chocolate Chip";
+            lblCookie_Name.ForeColor = Color.Turquoise;
+            pbMM_Cookie.Visible = false;
+            pbLemon_Cookie.Visible = false;
+            pbFrosted.Visible = false;
+            pbGinger.Visible = false;
+            pbSugar.Visible = false;
+            pcChoc_Cookie.Visible = true;
+        }
+        #endregion METHODS
+
+        #region EVENTS
+        
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            cookieclicker();
+        }
+        private void Btnlemon_Click(object sender, EventArgs e)
+        {
+            Set_Lemon_Cookie();
+        }
+
+        private void Btnfrost_Click(object sender, EventArgs e)
+        {
+            Set_Frosted_Cookie();
+        }
+
+        private void Btnsugar_Click(object sender, EventArgs e)
+        {
+            Set_Sugar_Cookie();
+        }
+
+        private void Btnmm_Click(object sender, EventArgs e)
+        {
+            Set_MM_Cookie();
+        }
+
+        private void Btnginger_Click(object sender, EventArgs e)
+        {
+            Set_ginger_Cookie();
+        }
+
+        private void BtnDirections_Click(object sender, EventArgs e)
+        {
+            DirectionForm form = new DirectionForm();
+            form.ShowDialog();
+
+        }
+        private void BtnChoc_Click(object sender, EventArgs e)
+        {
+            Set_Choc_Cookie();
+        }
+        #endregion EVENTS
+
+
+    }
+}
